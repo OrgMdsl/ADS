284f4190a416b740a50a7f1a8f42fb476c32bf9f
Gabriel <Gabriel@Gabriel-PC>
2015-10-25 20:50:38 -0200

    Criei todas as classes

---
+++ ProjetoADS5/src/java/Business/Interface/IContratoMaquinaBll.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Interface;
+
+import Business.Entity.Map.ContratoMaquina;
+import DataAccess.Utils.Helpers.IDalHelper;
+
+/**
+ *
+ * @author Gabriel
+ */
+public interface IContratoMaquinaBll extends IDalHelper<ContratoMaquina> {
+    
+}
---
+++ ProjetoADS5/src/java/DataAccess/Interface/IObjetoAtributoDal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAccess.Interface;
+
+import Business.Entity.Map.ObjetoAtributo;
+import DataAccess.Utils.Helpers.IDalHelper;
+
+/**
+ *
+ * @author Gabriel
+ */
+public interface IObjetoAtributoDal extends IDalHelper<ObjetoAtributo> {
+    
+}
